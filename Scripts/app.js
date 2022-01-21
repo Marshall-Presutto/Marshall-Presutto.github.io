f9c70f959cc3640bc8458228f2aeafd2a31e4c7e
Marshall Presutto <marshpresutto@gmail.com>
2022-01-21 15:33:34 -0500

    Updated Live site for ICE2

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,6 +9,40 @@
         {
             location.href = "about.html";
         })
+
+        // 1 get a reference to an entry point(s)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+
+        // 2 create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+
+        // 3 configure new element(s)
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+
+        let FirstParagraphString = "This is";
+        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`
+
+        MainParagraph.textContent = SecondParagraphString;
+        Article.setAttribute("class", "container");
+
+        // 4 add / insert new element(s)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+        
+        // Deletion example
+        //document.getElementById("ArticleParagraph").remove();
+
+        // Insert Before example
+        // let NewH1 = document.createElement("h1");
+        // NewH1.setAttribute("class", "display-1");
+        // NewH1.textContent = "Hello, World!";
+        // MainContent.before(NewH1);
     }
 
 
