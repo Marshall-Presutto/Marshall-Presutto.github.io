e1c965fd71e67da1e63135071e1f59fea89ad6fd
Marshall Presutto <marshpresutto@gmail.com>
2022-01-28 22:10:41 -0500

    updated live site

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,7 @@
 // AKA Anonymous Self-Executing Function
 (function()
 {
+
     function DisplayAboutPage()
     {
         console.log("About Us Page");
@@ -17,41 +18,45 @@
         console.log("Services Page");
     }
 
+
     function DisplayHomePage()
     {
         console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
+        
         AboutUsButton.addEventListener("click", function()
         {
+            // redirect to about page
             location.href = "about.html";
-        })
+        });
 
-        // 1 get a reference to an entry point(s)
+        // Step 1 get a reference to an entry point(s) (insertion point / deletion point)
         let MainContent = document.getElementsByTagName("main")[0];
         let DocumentBody = document.body;
-
-        // 2 create an element(s) to insert
+        
+        // Step 2 create an element(s) to insert
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
         let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
 
-
-        // 3 configure new element(s)
+        // Step 3 configure new element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
 
         let FirstParagraphString = "This is";
-        // example of template string
-        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`
+        // example of Template String
+        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`;
 
         MainParagraph.textContent = SecondParagraphString;
         Article.setAttribute("class", "container");
 
-        // 4 add / insert new element(s)
+        // Step 4 add / insert new element
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
-        
+
+
         // Deletion example
         //document.getElementById("ArticleParagraph").remove();
 
@@ -60,7 +65,6 @@
         // NewH1.setAttribute("class", "display-1");
         // NewH1.textContent = "Hello, World!";
         // MainContent.before(NewH1);
-
     }
 
     function DisplayContactPage()
@@ -70,15 +74,9 @@
         let sendButton = document.getElementById("sendButton");
         let subscribeCheckbox = document.getElementById("subscribeCheckbox");
 
-        // localStorage.setItem("1", "Tom");
-        // console.log(localStorage.getItem("1"));
-        // //localStorage.removeItem("1");
-        // console.log(localStorage.length);
-
-
         sendButton.addEventListener("click", function(event)
         {
-            // event.preventDefault(); // for debugging
+            //event.preventDefault();
 
             if(subscribeCheckbox.checked)
             {
@@ -86,7 +84,7 @@
                 if(contact.serialize())
                 {
                     let key = contact.FullName.substring(0, 1) + Date.now();
-                    
+
                     localStorage.setItem(key, contact.serialize());
                 }
             }
@@ -97,9 +95,9 @@
     {
         console.log("Contact-List Page");
 
-        if(localStorage.length > 0)
+        if(localStorage.length > 0) // check if localStorage has something in it 
         {
-            let conactList = document.getElementById("contactList");
+            let contactList = document.getElementById("contactList");
 
             let data = "";
 
@@ -107,12 +105,12 @@
 
             let index = 1;
 
-            // for ever key in the keys collection loop
+            //for every key in the keys collection loop
             for(const key of keys)
             {
                 let contactData = localStorage.getItem(key); // retrieve contact data from localStorage
 
-                let contact = new Contact(); // create an empty Contact object
+                let contact = new Contact(); // create an empty Contact Object
                 contact.deserialize(contactData);
 
                 data += `<tr>
@@ -122,10 +120,12 @@
                 <td>${contact.EmailAddress}</td>
                 <td></td>
                 <td></td>
-                </tr>`;
-
+                </tr>
+                `;
+                
                 index++;
             }
+
             contactList.innerHTML = data;
         }
     }
@@ -135,31 +135,28 @@
     {
         console.log("App Started!!");
 
-        switch(document.title)
+        switch (document.title) 
         {
-            case "Home":
-                DisplayHomePage();
-                break;
-            case "Contact Us":
-                DisplayContactPage();
-                break;
-            case "Contact-List":
-                DisplayContactListPage();
-                break;
-            case "About Us":
-                DisplayAboutPage();
-                break;
-            case "Products":
-                DisplayProductsPage();
-                break;
-            case "Services":
-                DisplayServicesPage();
-                break;
+          case "Home":
+            DisplayHomePage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Products":
+            DisplayProductsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
         }
-
-        
     }
-
 
     window.addEventListener("load", Start);
 
