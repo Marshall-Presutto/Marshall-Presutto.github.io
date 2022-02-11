8feaf02d6ed95d98c844b2f9476e93dd56edea94
Marshall Presutto <marshpresutto@gmail.com>
2022-02-11 15:31:32 -0500

    added files for ICE5 to live site

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,229 +1,342 @@
-//Partner 1: 
-    //Name:Ethan Bentolila
-    //Student ID:100783477 
-
-// Partner 2
-    // Name: Marshall Presutto
-    // Student ID: 100775601
-
-//Date Completed: 2022-02-03
-
+// IIFE -- Immediately Invoked Function Expression
+// AKA Anonymous Self-Executing Function
 (function()
 {
-
-    //Named function
-    function Start() 
+    function DisplayAboutPage()
     {
-        //Will always occur on all pages
-        switch(document.title) 
+        console.log("About Us Page");
+    }
+
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
+
+    function DisplayHomePage()
+    {
+        console.log("Home Page");
+        // old way
+        //let AboutUsButton = document.getElementById("AboutUsButton");
+        // AboutUsButton.addEventListener("click", function()
+        // {
+        //     location.href = "about.html";
+        // })
+
+        // Fattest memory footprint- need jquery
+        // jQuery way - returns all elements that contain an id of AboutUsButton - atach the "click" event to each of them
+        $("#AboutUsButton").on("click", function()
+        {
+            location.href = "about.html";
+        });
+
+        // second fattest memory footprint - getting stuff we dont need
+        // JavaScript way
+        // document.querySelectorAll("#AboutUsButton").forEach(function(element)
+        // {
+        //     element.addEventListener("click", function()
+        //     {
+        //         location.href = "about.html";
+        //     })
+        // });
+
+        // pretty lean
+        // document.querySelector("#AboutUsButton").addEventListener("click", function()
+        // {
+        //     location.href = "about.html";
+        // })
+
+        // Leanest - native javascript - only returning a reference to the object required
+        // document.getElementById("AboutUsButton").addEventListener("click", function()
+        // {
+        //     location.href = "about.html";
+        // })
+
+        // 1 get a reference to an entry point(s)
+        // let MainContent = document.getElementsByTagName("main")[0];
+        // let DocumentBody = document.body;
+
+        // 2 create an element(s) to insert
+        //let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+
+        // 3 configure new element(s)
+        //MainParagraph.setAttribute("id", "MainParagraph");
+        //MainParagraph.setAttribute("class", "mt-3");
+
+        // let FirstParagraphString = "This is";
+        // // example of template string
+        // let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`
+
+        // MainParagraph.textContent = SecondParagraphString;
+        // Article.setAttribute("class", "container");
+
+        // 4 add / insert new element(s)
+        $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
+        //Article.innerHTML = ArticleParagraph;
+        $("body").append(`<article class="container">
+        <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>
+        </article>`);
+
+        
+        // Deletion example
+        //document.getElementById("ArticleParagraph").remove();
+
+        // Insert Before example
+        // let NewH1 = document.createElement("h1");
+        // NewH1.setAttribute("class", "display-1");
+        // NewH1.textContent = "Hello, World!";
+        // MainContent.before(NewH1);
+
+    }
+
+    function AddContact(fullName, contactNumber, EmailAddress)
+    {
+        let contact = new core.Contact(fullName, contactNumber, EmailAddress);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+                    
+                    localStorage.setItem(key, contact.serialize());
+                }
+    }
+
+    /**
+     * This method validates an input text field in the form and
+     * displays an error in the messageArea div element
+     * @param {string} input_field_ID 
+     * @param {RegExp} regular_expression 
+     * @param {string} error_message 
+     */
+    function ValidateField(input_field_ID, regular_expression, error_message)
+    {
+        let messageArea = $("#messageArea").hide();
+
+        $("#" + input_field_ID).on("blur", function()
+        {
+            let = inputTextField = $(this).val();
+            if(!regular_expression.test(inputTextField))
+            {
+                $(this).trigger("focus").trigger("select");
+                messageArea.addClass("alert alert-danger").text(error_message).show();
+            }
+            else
+            {
+                messageArea.removeAttr("class").hide();
+            }
+        }); 
+    }
+
+    function ContactFormValidation()
+    {
+        ValidateField("fullName", /^([A-Z][a-z]{1,3}.?\s)?([A-Z][a-z]{1,25})+(\s|,|-)([A-Z][a-z]{1,25})+(\s|,|-)*$/, "Please enter a valid Full Name. This must include a capitalized first name followed by a capitalized last name.");
+        ValidateField("contactNumber", /^(\+\d{1,3}[\s-.]?)?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]?\d{4}$/, "Please enter a valid Contact number. Example: (905) 555 5555");
+        ValidateField("emailAddress", /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/, "Please enter a valid Email address.");
+
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Us Page");
+
+        ContactFormValidation();
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        // localStorage.setItem("1", "Tom");
+        // console.log(localStorage.getItem("1"));
+        // //localStorage.removeItem("1");
+        // console.log(localStorage.length);
+
+
+        sendButton.addEventListener("click", function(event)
+        {
+            // event.preventDefault(); // for debugging
+
+            if(subscribeCheckbox.checked)
+            {
+                AddContact(fullName.value, contactNumber.value, emailAddress.value);
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+
+            // for ever key in the keys collection loop
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key); // retrieve contact data from localStorage
+
+                let contact = new core.Contact(); // create an empty Contact object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td class="text-center"><button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button></td>
+                <td class="text-center"><button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button></td>
+                </tr>`
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+
+            $("#addButton").on("click", () =>
+            {
+                location.href = "edit.html#add";
+            });
+
+            $("button.delete").on("click", function()
+            {
+                if(confirm("Are you sure?"))
+                {
+                    localStorage.removeItem($(this).val());
+                }
+
+                location.href = "contact-list.html";
+            });
+
+            $("button.edit").on("click", function()
+            {
+                location.href = "edit.html#" + $(this).val();
+            });
+        }
+    }
+
+    function DisplayEditPage()
+    {
+        console.log("Edit Page");
+
+        ContactFormValidation();
+
+        let page = location.hash.substring(1);
+
+        switch(page)
+        {
+            case "add":
+                {
+                    $("main>h1").text("Add Contact");
+
+                    $("#editButton").html(`<i class="fas fa-plus-circle fa-lg"></i> Add`);
+
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+
+                        AddContact(fullName.value, contactNumber.value, emailAddress.value);
+
+                        location.href = "contact-list.html";
+                    });
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href = "contact-list.html";
+                    });
+                }
+                break;
+            default:
+                {
+                    let contact = new core.Contact();
+                    contact.deserialize(localStorage.getItem(page));
+
+                    $("#fullName").val(contact.FullName);
+                    $("#contactNumber").val(contact.ContactNumber);
+                    $("#emailAddress").val(contact.EmailAddress);
+
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+
+                        contact.FullName = $("#fullName").val();
+                        contact.ContactNumber = $("#contactNumber").val();
+                        contact.EmailAddress = $("#emailAddress").val();
+
+                        localStorage.setItem(page, contact.serialize());
+
+                        location.href = "contact-list.html";
+                    });
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href = "contact-list.html";
+                    });
+                }
+                break;
+        }
+    }
+
+    function DisplayLoginPage()
+    {
+        console.log("Login Page");
+    }
+
+    function DisplayRegisterPage()
+    {
+        console.log("Register Page");
+    }
+
+    // named function
+    function Start()
+    {
+        console.log("App Started!!");
+
+        switch(document.title)
         {
             case "Home":
                 DisplayHomePage();
                 break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+            case "Contact List":
+                DisplayContactListPage();
+                break;
             case "About Us":
                 DisplayAboutPage();
                 break;
-            case "Projects":
-                DisplayProjecstPage();
+            case "Products":
+                DisplayProductsPage();
                 break;
             case "Services":
                 DisplayServicesPage();
                 break;
-            case "Contact Us":
-                DisplayContactPage()
+            case "Edit":
+                DisplayEditPage();
+                break;
+            case "Register":
+                DisplayRegisterPage();
+                break;
+            case "Login":
+                DisplayLoginPage();
                 break;
         }
-        //called here as the navBar is to be added at the bottom
-        DisplayOnAllPages();
 
+        
     }
-    
+
+
     window.addEventListener("load", Start);
 
 })();
-
-
-
-//called if user is on any page
-function DisplayOnAllPages() {
-    //Change products to projects
-    let Products = document.getElementsByTagName("li")[1];
-    Products.innerHTML = Products.innerHTML.replace("Products","Projects");
-    Products.innerHTML = Products.innerHTML.replace("products","projects");
-
-
-    //Adds Human Resources to navbar
-    let Navbar = document.getElementsByTagName("ul")[0];
-    let ContactUs = document.getElementsByTagName("li")[4];
-    let HumanResources = document.createElement("li");
-    HumanResources.innerHTML = `<a class="nav-link" href="#"><i class="fas fa-user-circle"></i> Human Resources</a>`;
-    Navbar.insertBefore(HumanResources,ContactUs);
-
-
-    //Used to add the navbar
-    let DocumentBody = document.body;
-    let BottonNavBar = document.createElement("nav");
-    BottonNavBar.setAttribute("class","navbar fixed-bottom navbar-dark bg-dark");
-    BottonNavBar.innerHTML =`
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">&#169;Copyright ${new Date().getFullYear()}</a>
-    </div>
-    `;
-    DocumentBody.appendChild(BottonNavBar);
-
-
-}
-
-//called if the user is on the about page
-function DisplayAboutPage() {
-    document.getElementById("aboutHeader").textContent = "About Us";
-    document.getElementById("ethanName").textContent = "Ethan Bentolila";
-    document.getElementById("ethanDescription").innerHTML = `
-    Currently enrolled in the CPA program, I'm 19 years old with ambition to learn as much as I can about programming.
-    <br>I've been programming since I was 15 and have a decent grasp of java, c++, javascript, c# and python.
-    <br> In my spare time, I like to program, play basketball and socialize.
-    `;
-    document.getElementById("ethanResume").innerHTML = `Resume: <a href="./Assets/resume/Ethan-Resume.pdf" download>here</a>`;
-
-    document.getElementById("marshallName").textContent = "Marshall Presutto";
-    document.getElementById("marshallDescription").innerHTML = `
-    I am 25 years old and currently studying Computer Programming and Analysis at Durham College and determined to find a career working in Game Development.
-    <br>I have had an interest in videogames for as long as I can remember and look to apply my artistic abilities and programming skills in the field.
-    <br> Much of my spare time is spent playing videogames, I also enjoy going to the gym and sketching forms.
-        `;
-    document.getElementById("marshallResume").innerHTML = `Resume: <a href="./Assets/resume/Marshall-Resume.pdf" download>here</a>`;
-
-}   
-
-
-
-//called if the user is on the home page
-function DisplayHomePage() {
-    //DOM 
-    document.getElementById("title1").textContent = "Welcome to";
-    document.getElementById("title2").textContent = "Now";
-    document.getElementById("introParagraph").textContent = "Within this site you will find the future of Software and Web Development.";
-
-    //Used to display the contactForm once the page resets
-    if(localStorage.getItem("1")) {
-        let contactForm = new ContactForm();
-        contactForm.deserialize(localStorage.getItem("1"));
-        console.log(contactForm.toString());
-        localStorage.removeItem("1");
-    }
-
-
-
-}
-
-
-
-//called if the user is on the project page
-function DisplayProjecstPage() {
-    document.getElementById("projectHeader").textContent = "Projects Page";
-    document.getElementById("projectParagraph").textContent = "This page contains examples of projects completed by our team!";
-
-    document.getElementById("discordTitle").innerHTML = "Discord Bot";
-    document.getElementById("sudokuTitle").textContent = "Sudoku Solver";
-    document.getElementById("muralTitle").textContent = "Superhero Mural";
-
-
-    document.getElementById("discordBody").innerHTML = `
-    Discord bot. -used on a personal server that brings atmosphere to the server. features include:</br>
-    kick</br>
-    sending memes</br>
-    react for roles</br>
-    welcome message</br>
-    creating private channels</br>
-    providing solutions for puzzles in games
-    `;
-
-    document.getElementById("sudokuBody").innerHTML = `
-    Unfinished sudoku solver. Can solve a sudoku game simply by filling out the array with your sudokus known values.
-    </br> Finished product will have the ability to read your screen to autofill an array version of your sudoku board
-    `;
-
-    document.getElementById("muralBody").textContent = "Hand-drawn mock mural design based on the classic superhero Spider-man and his villain,Venom.";
-    
-
-    document.getElementById("sudokuDownload").innerHTML = `Program Code: <a href="./Assets/Projects/sudokubot.py" download>Download Now!</a>`;
-
-
-}
-
-
-//called if the user is on the project page
-function DisplayServicesPage() {
-    
-    document.getElementById("serviceHeader").textContent = "Services";
-    document.getElementById("serviceParagraph").textContent = "here is what we offer";
-    
-    document.getElementById("customProgHeader").textContent = "Custom Programming";
-    document.getElementById("customProgBody").textContent = "We offer the application of adept programming skills to develop in multiple languages as required. Projects in languages such as C#, C++, Python, Java, JavaScript and more are all achievable.";
-    
-    document.getElementById("tutorHeader").textContent = "Computer Programming Tutors";
-    document.getElementById("tutorBody").textContent = "We provide tutoring for various object-oreinted programming languages as well as programming concepts.";
-
-    document.getElementById("artHeader").textContent = "Art and Renderings";
-    document.getElementById("artBody").textContent = "We can provide custom hand drawn art and renderings for characters, digital options are also available.";
-
-
-    document.getElementById("ContactServices1").textContent = "Contact Now";
-    document.getElementById("ContactServices2").textContent = "Contact Now";
-    document.getElementById("ContactServices3").textContent = "Contact Now";
-
- 
-    // Logic for contact buttons redirect
-    let ContactServicesButton1 = document.getElementById("ContactServices1");
-
-    ContactServicesButton1.addEventListener("click", function()
-    {
-        location.href = "contact.html";
-    });
-
-    let ContactServicesButton2 = document.getElementById("ContactServices2");
-
-    ContactServicesButton2.addEventListener("click", function()
-    {
-        location.href = "contact.html";
-    });
-
-    let ContactServicesButton3 = document.getElementById("ContactServices3");
-
-    ContactServicesButton3.addEventListener("click", function()
-    {
-        location.href = "contact.html";
-    });
-
-}
-
-//called if the user is on the project page
-function DisplayContactPage() {
-    let SubmitButton = document.getElementById("Submit");
-
-    SubmitButton.textContent = "Send Message";
-
-    SubmitButton.addEventListener("click", function(event)
-    {
-
-        event.preventDefault();
-        let Name = document.getElementById("Name").value;
-        let Email = document.getElementById("Email").value;
-        let ContactNumber = document.getElementById("ContactNumber").value;
-        let Message = document.getElementById("Message").value;
-        newContactForm = new ContactForm(Name,Email,ContactNumber,Message);
-        console.log(newContactForm.toString());
-        
-        if(newContactForm.serialize()) {
-            let key = "1";
-            localStorage.setItem(key, newContactForm.serialize());
-        }
-
-        //Sets a timeout for 3 seconds then sends the user to a new page
-        setTimeout(function(){
-            location.href = "index.html";            
-        },3000);
-    
-    });
-
-}
